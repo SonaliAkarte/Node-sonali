cbe9ee40bb5cb941da502867f1771b6db3f1805a
sonali <sonali.akarte@kohler.com>
2022-08-05 10:08:25 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const http=require('http')
+const HOSTNAME='127.0.0.1';
+const PORT=3000;
+const server=http.createServer((req,res)=>{
+    res.statusCode=200;
+    res.setHeader('Content-type','text/plain');
+    res.end('Hello Sonali\n');
+
+});
+
+server.listen(PORT,HOSTNAME,()=>{
+    console.log(`Server is running ${HOSTNAME} on port ${PORT}`);
+});
