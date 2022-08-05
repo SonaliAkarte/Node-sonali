cbe344745d425529fcacdae32152c2caab47be7f
sonali <sonali.akarte@kohler.com>
2022-08-05 10:22:10 +0530

    Changed Commit

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const PORT=3000;
 const server=http.createServer((req,res)=>{
     res.statusCode=200;
     res.setHeader('Content-type','text/plain');
-    res.end('Hello Sonali\n');
+    res.end('Hello Sonali Akarte\n');
 
 });
 
 server.listen(PORT,HOSTNAME,()=>{
     console.log(`Server is running ${HOSTNAME} on port ${PORT}`);
-});
+}); 
