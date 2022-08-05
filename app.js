287db9e751c2e8bc91970f9e4d352ecc8fcc9ef4
sonali <sonali.akarte@kohler.com>
2022-08-05 10:31:52 +0530

    Changed Commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT=3000;
 const server=http.createServer((req,res)=>{
     res.statusCode=200;
     res.setHeader('Content-type','text/plain');
-    res.end('Hello Sonali Akarte\n');
+    res.end('Sonali Akarte\n');
 
 });
 
